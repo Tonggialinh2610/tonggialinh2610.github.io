af68cdb33941095eec90b1692e7b3417bf215379
Tonggialinh2610 <42639948+Tonggialinh2610@users.noreply.github.com>
2018-11-07 11:49:57 +0700

    Add files via upload

---
+++ bai re nhanh.js
@@ -0,0 +1,10 @@
+//bài 1
+function testNumber(a,b){
+	if(isNaN(a,b)){
+		console.log("đây không phải là number")}
+	else{
+		let c = Math.max(a,b); let d = Math.min(a,b);
+		console.log(c +" là số lớn hơn số" + d)
+	}
+}
+//bài 2
